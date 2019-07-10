7f910bfe5ec2a766d3de349371732815ea05126a
Hellowlol <Hellowlol@users.noreply.github.com>
2019-07-10 23:42:13 +0200

    Create test_make.py

---
+++ tests/test_make.py
@@ -0,0 +1,54 @@
+# just run the damn maketests
+# https://github.com/dpwe/audfprint/blob/master/Makefile
+
+
+def test_onecore():
+    pass
+
+
+def test_remove():
+    pass
+
+
+def test_fpdbase_pklz():
+    pass
+
+
+def test_test_onecore_precomp():
+    pass
+
+
+def test_test_onecore_newmerge():
+    pass
+
+
+def test_precompdir():
+    pass
+
+
+def test_onecore_precomppk():
+    pass
+
+
+def precomppkdir():
+    pass
+
+
+def test_mucore():
+    pass
+
+
+def test_fpdbase_mu_pklz():
+    pass
+
+
+def test_mucore_precomp():
+    pass
+
+
+def test_precompdir_mu():
+    pass
+
+
+def test_hash_mask():
+    pass
